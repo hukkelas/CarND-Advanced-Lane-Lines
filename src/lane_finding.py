d4b32e99b6f9359877c3e8e415469cfea33d7ca9
Håkon Hukkelås <haak-hu@hotmail.com>
2018-09-21 09:04:09 +0200

    add: finish initial pipeline

--- src/lane_finding.py
+++ src/lane_finding.py
@@ -1,6 +1,8 @@
 import cv2
 import glob
 import numpy as np
+import matplotlib
+matplotlib.use("Qt5Agg")
 import matplotlib.pyplot as plt
 from camera_calibration import compute_calibration_coefficients, undistort
 from thresholding import compute_binary_image
@@ -117,8 +119,7 @@ def fit_polynomial(binary_warped):
     out_img[righty, rightx] = [0, 0, 255]
 
     # Plots the left and right polynomials on the lane lines
-    plt.plot(left_fitx, ploty, color='yellow')
-    plt.plot(right_fitx, ploty, color='yellow')
+
 
     return out_img, left_fit, right_fit, ploty, left_fitx, right_fitx
 
@@ -144,11 +145,30 @@ def measure_curvature_pixels(ploty, left_fit, right_fit):
     
     return left_curverad, right_curverad
 
+def map_lane_lines_to_original(img, warped, left_fitx, right_fitx, ploty, Minv):
+    warp_zero = np.zeros_like(warped).astype(np.uint8)
+    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+    # Recast the x and y points into usable format for cv2.fillPoly()
+    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+    pts = np.hstack((pts_left, pts_right))
+
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
+
+    # Warp the blank back to original image space using inverse perspective matrix (Minv)
+    newwarp = cv2.warpPerspective(color_warp, Minv, (img.shape[1], img.shape[0])) 
+    # Combine the result with the original image
+    result = cv2.addWeighted(img, 1, newwarp, 0.3, 0)
+    return result
+
+
 if __name__ == '__main__':
     test_image_files = glob.glob('test_images/*.jpg')
     dist, mtx = compute_calibration_coefficients()
     for f in test_image_files:
-        plt.figure(figsize=(20,20))
+        plt.figure(figsize=(10,10))
         img = cv2.imread(f)
 
         img = undistort(img, mtx, dist)
@@ -164,27 +184,18 @@ if __name__ == '__main__':
         leftx, lefty, rightx, righty, out_img = find_lane_pixels(warped)
         plt.subplot(2,2,2)
         plt.imshow(out_img)
+  
 
         out_img, left_fit, right_fit, ploty, left_fitx, right_fitx = fit_polynomial(warped)
+                
         plt.subplot(2,2,3)
+        plt.plot(left_fitx, ploty, color='yellow')
+        plt.plot(right_fitx, ploty, color='yellow')      
         plt.imshow(out_img)
         left_curverad, right_curverad = measure_curvature_pixels(ploty, left_fit, right_fit)
         plt.subplot(2,2,4)
-        warp_zero = np.zeros_like(warped).astype(np.uint8)
-        color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+        result = map_lane_lines_to_original(img, warped, left_fitx, right_fitx, ploty, Minv)
 
-        # Recast the x and y points into usable format for cv2.fillPoly()
-        pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
-        pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
-        pts = np.hstack((pts_left, pts_right))
-
-        # Draw the lane onto the warped blank image
-        cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
-
-        # Warp the blank back to original image space using inverse perspective matrix (Minv)
-        newwarp = cv2.warpPerspective(color_warp, Minv, (img.shape[1], img.shape[0])) 
-        # Combine the result with the original image
-        result = cv2.addWeighted(img, 1, newwarp, 0.3, 0)
         plt.imshow(result)
         plt.show()
 
--- src/main.py
+++ src/main.py
@@ -7,3 +7,58 @@
 # Determine the curvature of the lane and vehicle position with respect to center.
 # Warp the detected lane boundaries back onto the original image.
 # Output visual display of the lane boundaries and numerical estimation of lane curvature and vehicle position.
+import cv2
+import glob
+import numpy as np
+import matplotlib
+matplotlib.use("Qt5Agg")
+import matplotlib.pyplot as plt
+from camera_calibration import compute_calibration_coefficients, undistort
+from thresholding import compute_binary_image
+from perspective_transform import perspective_projection
+from lane_finding import measure_curvature_pixels, fit_polynomial, map_lane_lines_to_original
+from moviepy.editor import VideoFileClip
+
+dist, mtx = compute_calibration_coefficients()
+
+
+
+def find_lines(img):
+  img = undistort(img, mtx, dist)
+  
+  gray = compute_binary_image(img)
+  warped, M = perspective_projection(gray)
+  Minv = np.linalg.inv(M)
+
+  out_img, left_fit, right_fit, ploty, left_fitx, right_fitx = fit_polynomial(warped)
+  left_curverad, right_curverad = measure_curvature_pixels(ploty, left_fit, right_fit)
+  result = map_lane_lines_to_original(img, warped, left_fitx, right_fitx, ploty, Minv)
+  return result
+
+
+
+def process_video(input_path, output_path="output_images/test.mp4"):
+  ## To speed up the testing process you may want to try your pipeline on a shorter subclip of the video
+  ## To do so add .subclip(start_second,end_second) to the end of the line below
+  ## Where start_second and end_second are integer values representing the start and end of the subclip
+  ## You may also uncomment the following line for a subclip of the first 5 seconds
+  ##clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4").subclip(0,5)
+  clip1 = VideoFileClip(input_path)
+  white_clip = clip1.fl_image(find_lines) #NOTE: this function expects color images!!
+  white_clip.write_videofile(output_path, audio=False)
+
+
+if __name__ == '__main__':
+  if True:
+    #process_video("project_video.mp4")
+    #process_video("challenge_video.mp4", 'challenge_video_out.mp4')
+    process_video("harder_challenge_video.mp4", "output_images/harder_challenge_video_out.mp4")
+  else:
+    test_image_files = glob.glob('test_images/*.jpg')
+    dist, mtx = compute_calibration_coefficients()
+    for f in test_image_files:
+        plt.figure(figsize=(10,10))
+        img = cv2.imread(f)
+        result = find_lines(img)
+        plt.imshow(result)
+        plt.show()
